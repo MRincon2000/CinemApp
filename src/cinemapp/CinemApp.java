21ce13f3b7098b3b1e2f59e7276d283afcb47f4b
MSI PC <MSI PC@192.168.0.175>
2020-04-24 18:09:43 -0500

    Version 1.0 alpha-Implementado en listas enlazadas y listas en arreglos

--- src/cinemapp/CinemApp.java
+++ src/cinemapp/CinemApp.java
@@ -16,32 +16,31 @@ public class CinemApp {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Cine cinepolis=new Cine(10);
-        Pelicula p =new Pelicula("Narutico","Sala 1");
-        Pelicula p1= new Pelicula("Titanic", "Sala 2");
-        cinepolis.insertar(p);
-        cinepolis.insertar(p1);
-        System.out.println(cinepolis.getPeliculas()[0].getNombre());
-        cinepolis.getPeliculas()[0].mostrar();
-        cinepolis.getPeliculas()[0].getHorarios()[0].MostrarDisponibles();
-        
-        for(int e=0;e<7;e++){
-        for (int i=0;i<99;i++){
-        cinepolis.getPeliculas()[0].getHorarios()[e].ComprarSilla("El amante",i);
-        }
-        }
-        for(int u=0;u<7;u++){
-        for (int o=0;o<99;o++){
-        cinepolis.getPeliculas()[1].getHorarios()[u].ComprarSilla("Tu amoroso",o);
-        }
-        }
-        
-        cinepolis.getPeliculas()[0].getHorarios()[0].MostrarTodas();
-        cinepolis.getPeliculas()[0].getHorarios()[0].MostrarDisponibles();
-        cinepolis.getPeliculas()[1].getHorarios()[0].MostrarTodas();
-        cinepolis.getPeliculas()[1].getHorarios()[0].MostrarDisponibles();
-        
-        
-    }
+ CineLinked cinepolis=new CineLinked();      
+   PeliculaLinked p= new PeliculaLinked("Narutin","Sala 1") ;
+ cinepolis.insert(p);
+
+try{
+    System.out.println(cinepolis.BuscarPeli("Narut").getSala());
+}catch(NullPointerException e){
+    System.out.println("No se ecuentra la peli");
+}
+    System.out.println(cinepolis.BuscarPeli("Narutin").getSala());
+
+  
     
+    try{  
+    
+    cinepolis.BuscarPeli("Narutin").BuscarHorario(15).ComprarSilla("Ronalt el Killa", 9);
+    cinepolis.BuscarPeli("Narutin").BuscarHorario(15).MostrarDisponibles();
+    cinepolis.BuscarPeli("Narutin").BuscarHorario(15).MostrarTodas();
+    
+  }catch(NullPointerException l){
+      System.out.println("No se ecuentra el horario");
+  }
+   
+   
+   
+   
+}
 }
